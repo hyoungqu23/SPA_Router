3374c516ae50a3f67f28a1b03fa348ff50b9cdea
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-07 01:39:57 +0900

    Feat: useRouter 버튼 추가

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Link from '../../components/Router/Link';
+import Link from '../../Router/Link';
+import { useRouter } from '../../hooks/useRouter';
 
 const About = () => {
+  const { push } = useRouter();
+
+  const handleBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    push({ url: '/' });
+  };
+
   return (
     <div>
       <h1>About</h1>
       <Link href="/">Move to Root</Link>
+      <button onClick={handleBtnClick}>Move by Hook</button>
     </div>
   );
 };
